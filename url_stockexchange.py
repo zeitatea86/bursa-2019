7857c01093a47137284dc2f0252766ca8791323a
zeitatea86 <mihaitza_master@yahoo.com>
2021-06-17 17:44:30 +0300

    Add new comment

--- url_stockexchange.py
+++ url_stockexchange.py
@@ -26,6 +26,7 @@ def timp():
 
 def obtainStock(stock):
     url="https://finance.yahoo.com/quote/"+stock+"?p="+stock
+    print(url)
     data=urllib.request.urlopen(url).read()
     data1=data.decode("UTF-8")
     m=re.search("regularMarketPrice",data1)
